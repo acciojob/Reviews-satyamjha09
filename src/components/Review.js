aed1dd8b7daebf03d1620d38bd19dffcecb6389f
satyamjha <satyamjhaactor07@gmail.com>
2025-02-19 11:00:08 +0530

    Done Project_1

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,79 +1,44 @@
 import React, { useState } from 'react'
-
-
-const reviews = [
-    {
-      id: 1,
-      name: 'Susan Smith',
-      job: 'Web Developer',
-      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse...",
-    },
-    {
-      id: 2,
-      name: 'Anna Johnson',
-      job: 'Web Designer',
-      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
-      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle...',
-    },
-    {
-      id: 3,
-      name: 'Peter Jones',
-      job: 'Intern',
-      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-      text: 'Sriracha literally flexitarian irony, vape marfa unicorn...',
-    },
-    {
-      id: 4,
-      name: 'Bill Anderson',
-      job: 'The Boss',
-      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom...',
-    }
-  ];
-  
+import reviews from './data';
 
 const Review = () => {
 
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
+  const {id , name , job , image, text} = reviews[index];
 
+  const prevReview = () => {
+    setIndex((prevIndex) => (prevIndex === 0 ? reviews.length - 1 : prevIndex -1));
+  }
 
-    const prevReview = () => {
-        setIndex((prev) => (prev === 0 ? reviews.length - 1 : prev - 1));
-    };
+  const nextReview = () => {
+    setIndex((prevIndex) => (prevIndex === reviews.length - 1 ? 0 : prevIndex + 1));
+  }
 
-    const nextReview = () => {
-        setIndex((prev) => (prev === reviews.length - 1 ? 0 : prev + 1));
-    };
+  const randomReview = () => {
+    let randomIndex;
 
-    const randomReview = () => {
-        let newIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * reviews.length);
 
-        do {
-            newIndex = Math.floor(Math.random()* reviews.length);
-        } while(newIndex === index);
-
-        setIndex(newIndex);
-    };
-
-    const {id, name, job, image, text } = reviews[index];
-
+    } while(randomIndex === index){
+      setIndex(randomIndex);
+    }
+  } 
 
   return (
     <article className="review">
-      <div className="image">
-        <img src={image} alt={name} className="person-img" />
-      </div>
-      <h3 className="author" id={`author-${id}`}>{name}</h3>
-      <h4 className="job">{job}</h4>
-      <p className="info">{text}</p>
+      <img src={image} alt={name} className="person-img" />
+      <h4 id={`author${id}`} className="author"> {name} </h4>
+      <p className="job"> {job} </p>
+      <p className="info"> {text} </p>
 
       <div>
-        <button className="prev-btn" onClick={prevReview}>Prev</button>
-        <button className="next-btn" onClick={nextReview}>Next</button>
+        <button className="prev-btn" onClick={prevReview}> Prev </button>
+        <button className="next-btn" onClick={nextReview}> Next </button>
       </div>
 
-      <button className="random-btn" onClick={randomReview}>Surprise Me</button>
+      <button className="random-btn" onClick={randomReview}> Surprice Me </button>
+
     </article>
   )
 }
---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text: "I'm baby meggings twee health goth +1. Bicycle rights turmeric chartreuse before they sold out chambray pop-up...",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen...",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated...",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo...",
+    },
+  ];
+  
+  export default reviews;
+  
