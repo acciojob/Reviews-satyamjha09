c1a4e7325ec81b1a9d79ac9021c75289309cfc5b
satyamjha <satyamjhaactor07@gmail.com>
2025-03-01 19:51:30 +0530

    Done Project

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,46 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import reviews from './data';
 
 const Review = () => {
-
   const [index, setIndex] = useState(0);
-  const {id , name , job , image, text} = reviews[index];
+  const { id, name, job, image, text } = reviews[index];
 
-  const prevReview = () => {
-    setIndex((prevIndex) => (prevIndex === 0 ? reviews.length - 1 : prevIndex -1));
-  }
+  const checkIndex = (num) => {
+    if (num < 0) return reviews.length - 1;
+    if (num >= reviews.length) return 0;
+    return num;
+  };
 
-  const nextReview = () => {
-    setIndex((prevIndex) => (prevIndex === reviews.length - 1 ? 0 : prevIndex + 1));
-  }
-
+  const nextReview = () => setIndex((prevIndex) => checkIndex(prevIndex + 1));
+  const prevReview = () => setIndex((prevIndex) => checkIndex(prevIndex - 1));
   const randomReview = () => {
-    let randomIndex;
-
+    let newIndex;
     do {
-      randomIndex = Math.floor(Math.random() * reviews.length);
-
-    } while(randomIndex === index){
-      setIndex(randomIndex);
-    }
-  } 
+      newIndex = Math.floor(Math.random() * reviews.length);
+    } while (newIndex === index);
+    setIndex(newIndex);
+  };
 
   return (
     <article className="review">
       <img src={image} alt={name} className="person-img" />
-      <h4 id={`author${id}`} className="author"> {name} </h4>
-      <p className="job"> {job} </p>
-      <p className="info"> {text} </p>
-
-      <div>
-        <button className="prev-btn" onClick={prevReview}> Prev </button>
-        <button className="next-btn" onClick={nextReview}> Next </button>
+      <h4 id={`author-${id}`} className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevReview}>&lt;</button>
+        <button className="next-btn" onClick={nextReview}>&gt;</button>
       </div>
-
-      <button className="random-btn" onClick={randomReview}> Surprice Me </button>
-
+      <button className="random-btn" onClick={randomReview}>Surprise Me</button>
     </article>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
